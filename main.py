a3a9fb32c6819ed544f55f8c0b4c5ec565449f9a
Ananya-Bhardwaj <ananya.bhar@gmail.com>
2024-11-29 19:39:31 +0530

    Added route files init

--- main.py
+++ main.py
@@ -2,20 +2,7 @@ from src.utils import connect_db
 
 app, db = connect_db()
 
-@app.route('/')
-def home():
-    db.test.insert_one({'name': 'Flask'})
-    # Get first document from the collection
-    a = db.test.find_one({'name': 'Flask'})
-    print(a)
-    # Get all documents from the collection
-    b = db.test.find({'name': 'Flask'})
-    for item in b:
-        print(item)
-        # access field
-        print(item['name'])
-        print(item['_id'])
-    return "Hello, Flask!"
+import src.routes
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/routes.py
+++ src/routes.py
@@ -1,11 +1,4 @@
-from flask import Blueprint
-from src.models.paper import paper_model
-# from src.models.user_poly_values import user_poly_model
-# from src.models.user_shares import user_shares_model
-from src.models.user import user_model
-
-# main blueprint to be registered with application
-api = Blueprint('api', __name__)
-
-api.register_blueprint(user_model, url_prefix="/users") # http://localhost:5000/api/users
-api.register_blueprint(paper_model, url_prefix="/papers") # http://localhost:5000/api/papers
+import src.controllers.paper_route
+import src.controllers.user_poly_values_route
+import src.controllers.user_route
+import src.controllers.user_shares_route
